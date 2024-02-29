44eadfeb0b6610f8d75ab4ac575f7bb820b66ee6
Avadhut lakule <lakuleavadhut@users.noreply.github.com>
2024-02-29 11:00:23 +0530

    Initial push

---
+++ src/pages/Checkout.js
@@ -0,0 +1,39 @@
+import React, {useContext} from 'react';
+
+import { useParams } from 'react-router-dom';
+
+import {CartContext} from '../contexts/CartContext';
+
+import {ProductContext} from '../contexts/ProductContext';
+
+const Checkout = () => {
+  const {id} = useParams(); 
+  const products = useContext(ProductContext);
+  const {clearCart} = useContext(CartContext);
+
+  const product = products.find((item) => {
+    return item.id === parseInt(id);
+  })
+
+  const { title, price, description, image } =  product;
+
+  return(
+    <section className='pt-32 pb-12 lg:py-32h-screen flex items-center'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col lg:flex-row items-center'>
+            <div className='flex flex-1 justify-center items-center mb-8 lg:mb-0'>
+              <img className='max-w-[200px] lg:max-w-sm' src ={image} alt=''/>
+            </div>
+            <div className='flex-1 text-center lg:text-left'>
+              <h1 className='text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0'>{title}</h1>
+              <div className='text-xl text-red-500 font-medium mb-6'> $ {price}</div>
+              <p className='mb-8'>{description}</p>
+              <button onClick={() =>{clearCart()}} className='bg-primary py-4 px-8 text-white'>Confirm Order</button>
+            </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Checkout;
